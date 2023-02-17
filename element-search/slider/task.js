9f892b5da8f94f523163f85f63a4ab0ddb62a00d
Maria Bugaeva <116347809+Shpalkin@users.noreply.github.com>
2023-02-17 09:28:48 +0300

    Update task.js

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,38 +1,28 @@
-const slide = Array.from(document.querySelectorAll('.slider__arrow'));
-const item = Array.from(document.querySelectorAll('.slider__item'));
-const dots = Array.from(document.querySelectorAll('.slider__dot'));
-let image = 0;
-dots[0].classList.add('slider__dot_active');
+const slides = document.querySelectorAll('.slider__item');
 
-slide.forEach((b) => {
-  b.addEventListener('click', handler);
-});
-dots.forEach((b) => {
-  b.addEventListener('click', dotsClick);
-});
+const next = document.querySelector('div.slider__arrow.slider__arrow_next');
+const previous = document.querySelector('div.slider__arrow.slider__arrow_prev');
 
-function dotsClick(){
-  remove(image)
-  this.classList.add("slider__dot_active");
-  image = dots.findIndex(it => it == this);
-  item[image].classList.add("slider__item_active");
-}
+let currentSlide = 0;
 
-function remove(){
-  item[image].classList.remove("slider__item_active");
-  dots[image].classList.remove("slider__dot_active");
-}
+next.onclick = function() {
+  slides[currentSlide].className = 'slider__item';
+  if (currentSlide < slides.length - 1) { 
+    currentSlide++;
+  } 
+  else {
+    currentSlide = 0;
+  }    
+  slides[currentSlide].className = 'slider__item slider__item_active';
+};
 
-function handler(){
-  remove(image)
-  if (this.className == 'slider__arrow slider__arrow_next'){
-    image++;
-    if(image === item.length){image = 0};
-  };
-  if (this.className == 'slider__arrow slider__arrow_prev'){
-    image -=1;
-    if (image == -1) {image = item.length-1};
-  };
-  item[image].classList.add("slider__item_active");
-  dots[image].classList.add("slider__dot_active");
-}
+previous.onclick = function() {
+    slides[currentSlide].className = 'slider__item';
+    if (currentSlide > 0) { 
+      currentSlide--;
+    } 
+    else {
+      currentSlide = slides.length - 1;
+    }    
+    slides[currentSlide].className = 'slider__item slider__item_active';
+};
