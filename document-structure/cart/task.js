4bb9d356edd2e00bd94ee213249fbf35a621f562
Maria Bugaeva <116347809+Shpalkin@users.noreply.github.com>
2023-04-08 01:48:49 +0300

    Update task.js

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,63 +1,51 @@
-const cart = document.querySelector('.cart__products');
+'use strict'
 
-const productsList = document.querySelectorAll('.product');
+const cartProducts = document.querySelector('.cart__products')
+const productControlsEls = document.querySelectorAll(
+  '.product__quantity-control',
+)
+const addToCart = document.querySelectorAll('.product__add')
 
-getInfoAboutProduct = (item) => {
-  const controlsItem = item.querySelector('.product__controls');
-  const countBlock = controlsItem.querySelector('.product__quantity-value');
-  const imgItem = item.querySelector('.product__image').src;
-  const quantity = item.querySelectorAll('.product__quantity-control');
-  const dataId = item.getAttribute('data-id');
-
-  quantity.forEach(item => {
-    item.addEventListener('click', e => {
-      if (e.target.classList.contains('product__quantity-control_dec')) {
-        if (Number(countBlock.textContent) - 1 === 0) {
-          return;
-        } else {
-          countBlock.innerHTML = Number(countBlock.textContent) - 1;
-        }
-      } else {
-        countBlock.innerHTML = Number(countBlock.textContent) + 1;
-      }
-    });
-  });
-  const addBtn = controlsItem.querySelector('.product__add');
-
-
-
-  addBtn.addEventListener('click', () => {
-
-    const div = document.createElement('div');
-
-    div.setAttribute('data-id', dataId);
-    div.classList.add('cart__product');
-
-    div.innerHTML = `
-      <img class="cart__product-image" src=${imgItem}>
-      <div class="cart__product-count">${countBlock.textContent}</div>
-    `;
-
-    const cartList = cart.querySelectorAll('.cart__product');
-
-    if (cartList.length < 1) {
-      return cart.appendChild(div);
-    } else {
-      cartList.forEach(item => {
-        const idElem = item.getAttribute('data-id');
-        const countElem = item.querySelector('.cart__product-count');
-        if (idElem === dataId) {
-          item.remove();
-          div.querySelector('.cart__product-count').innerHTML = countElem.innerHTML = Number(countElem.textContent) + Number(countBlock.textContent);
-        } else {
-          return cart.appendChild(div)
-        }
-      });
+productControlsEls.forEach((control) => {
+  control.addEventListener('click', (e) => {
+    let currentControl = e.target
+    let parentProduct = e.target.closest('.product')
+    let counter = parentProduct.querySelector('.product__quantity-value')
+    if (currentControl.classList.contains('product__quantity-control_inc')) {
+      counter.innerText = ++counter.innerText
     }
+    if (currentControl.classList.contains('product__quantity-control_dec')) {
+      if (parseInt(counter.innerText) < 2) {
+        return false
+      }
+      counter.innerText = --counter.innerText
+    }
+  })
+})
 
-  });
-};
+addToCart.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    let currentBtn = e.target
+    let parentEl = currentBtn.closest('.product')
+    const productInfo = {
+      id: parentEl.dataset.id,
+      imgSrc: parentEl.querySelector('.product__image').getAttribute('src'),
+      quantity: parentEl.querySelector('.product__quantity-value').innerText,
+    }
+    const itemInCart = cartProducts.querySelector(
+      `[data-id = "${productInfo.id}"]`,
+    )
+    if (itemInCart) {
+      const counterEL = itemInCart.querySelector('.cart__product-count')
+      counterEL.innerText =
+        parseInt(counterEL.innerText) + parseInt(productInfo.quantity)
+    } else {
+      const cartItemHTML = `<div class="cart__product" data-id="${productInfo.id}">
+            <img class="cart__product-image" src="${productInfo.imgSrc}" />
+            <div class="cart__product-count">${productInfo.quantity}</div>
+          </div>`
 
-productsList.forEach((el) => {
-  getInfoAboutProduct(el);
-});
+      cartProducts.insertAdjacentHTML('beforeend', cartItemHTML)
+    }
+  })
+})
