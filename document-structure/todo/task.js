885188c64b6bdda0eaac829489d289436c31a0db
Maria Bugaeva <116347809+Shpalkin@users.noreply.github.com>
2023-04-08 01:47:19 +0300

    Update task.js

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,49 +1,34 @@
-'use strict';
+'use strict'
 
-document.addEventListener('DOMContentLoaded', function() {
-    const input = document.getElementById('task__input');
-    const tasksList = document.getElementById('tasks__list');
-    const storageKey = 'ToDo';
-    loadContent(storageKey);
-    // console.log(localStorage.getItem('ToDo'));
+const formTaskControl = document.getElementById('tasks__form')
+const taskInput = document.getElementById('task__input')
+const listEl = document.getElementById('tasks__list')
+let task, taskList, link
 
+formTaskControl.addEventListener('submit', (e) => {
+  e.preventDefault()
 
-    function deleteTaskEvent(task) {
-        task.querySelector('.task__remove').addEventListener('click', (e) => {
-            e.preventDefault();
-            task.remove();
-            saveContent(storageKey);
-        })
-    }
+  if (!taskInput.value) {
+    return false
+  }
+  task = document.createElement('div')
+  task.className = 'task'
+  listEl.appendChild(task)
+  taskList = document.createElement('div')
+  taskList.className = 'task__title'
+  task.appendChild(taskList)
+  taskList.innerHTML += taskInput.value
+  link = document.createElement('a')
+  link.href = '#'
+  link.className = 'task__remove'
+  link.appendChild(document.createTextNode('×'))
+  task.appendChild(link)
 
-
-    function saveContent(name) {
-        const string = JSON.stringify(tasksList.innerHTML);
-        localStorage.setItem(name, string);
-    }
-
-
-    function loadContent(name) {
-        const loaded = JSON.parse(localStorage.getItem(name));
-        tasksList.innerHTML = loaded;
-        const tasks = tasksList.querySelectorAll('.task');
-        tasks.forEach((task) => { deleteTaskEvent(task) });
-    }
-
-
-    input.addEventListener('change', function(e) {
-        if (input.value) {
-            const newTask = document.createElement('div');
-            newTask.classList.add('task');
-            newTask.innerHTML = `
-            <div class="task__title">
-              ${input.value}
-            </div>
-            <a href="#" class="task__remove">&times;</a>`;
-            tasksList.appendChild(newTask);
-            input.value = '';
-            deleteTaskEvent(newTask);
-            saveContent(storageKey);
-        };
-    });
+  link.addEventListener('click', (e) => {
+    e.preventDefault()
+    let currentLink = e.target
+    let deletedTask = currentLink.closest('.task')
+    deletedTask.remove()
+  })
+  taskInput.value = ''
 })
